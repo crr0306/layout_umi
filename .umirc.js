f1019bea9a3d5758f1c830743f8c29d879819cfe
crr0306 <2438167295@qq.com>
2020-08-31 10:28:31 +0800

    约定路由以及配置端口

--- .umirc.js
+++ .umirc.js
@@ -2,16 +2,16 @@
 // ref: https://umijs.org/config/
 export default {
   treeShaking: true,
-  // routes: [
-  //   {
-  //     path: '/',
-  //     component: '../layouts/index',
-  //     routes: [
-  //       { path: '/', component: '../pages/index' },
-  //       { path: '/login', component: '../pages/login/index' }
-  //     ]
-  //   }
-  // ],
+  routes: [
+    {
+      path: '/',
+      component: '../layouts/index',
+      routes: [
+        { path: '/', component: '../pages/index' },
+        { path: '/login', component: '../pages/login/index' }
+      ]
+    }
+  ],
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
     ['umi-plugin-react', {
